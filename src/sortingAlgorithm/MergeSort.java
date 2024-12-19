5b1c1e83e4cce3326620ba0d134b0360c1103925
dokozak <130685403+dokozak@users.noreply.github.com>
2024-12-19 12:42:19 +0100

    Complete the mergeSort but not have documentation

--- src/sortingAlgorithm/MergeSort.java
+++ src/sortingAlgorithm/MergeSort.java
@@ -1,15 +1,67 @@
 package sortingAlgorithm;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
-public class MergeSort<T extends Comparable<T>>{
+public class MergeSort<T extends Comparable<T>> {
 
-	public void sorting(T[] list) 
-	{
-		
-		
-		
+	public ArrayList<T> sorting(ArrayList<T> list) {
+		ArrayList<T> right = new ArrayList<>(), left = new ArrayList<>();
+
+		if (list.size() <= 1)
+			return list;
+		else {
+			float mid = list.size() / 2;
+			mid =  (mid / 1f == 0.5f) ?  mid-0.5f: mid;
+
+			for (int x = 0; x < mid; x++) {
+				left.add(list.get(x));
+				
+			}
+			for (int x = 0; x < list.size() - mid; x++) {
+				right.add(list.get((int) (x + mid)));
+			}
+
+			left = sorting(left);
+			right = sorting(right);
+
+			if (left.get(left.size() - 1).compareTo(right.get(0)) < 0) {
+
+				for (int x = 0; x < right.size(); x++) {
+					left.add(right.get(x));
+				}
+				return left;
+			}
+			return merge(left, right);
+		}
+
 	}
-	
-	
+
+	public ArrayList<T> merge(ArrayList<T> left, ArrayList<T> right) {
+		ArrayList<T> result = new ArrayList<>();
+		int x1 = 0, x2 = 0;
+
+		while (x1 != left.size() && x2 != right.size()) {
+			if (left.get(x1).compareTo(right.get(x2)) < 0) {
+				result.add(left.get(x1));
+				x1++;
+			} else {
+				result.add(right.get(x2));
+				x2++;
+			}
+		}
+		if (x1 != left.size()) {
+			while (x1 != left.size()) {
+				result.add(left.get(x1));
+				x1++;
+			}
+		} else {
+			while (x2 != right.size()) {
+				result.add(right.get(x2));
+				x2++;
+			}
+		}
+		return result;
+	}
+
 }
--- src/sortingAlgorithm/initiator.java
+++ src/sortingAlgorithm/initiator.java
@@ -1,5 +1,8 @@
 package sortingAlgorithm;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class initiator {
 	
 	//Type of sorting
@@ -7,21 +10,41 @@ public class initiator {
 	private static MergeSort<Integer> mergeSort = new MergeSort<Integer>();
 	
 	public static void main(String[] args) {
-		Integer[] list = {7};
+		//Integer[] list = {7, 1, 44, 666, 2, 4, 6, 8, 11};
+		ArrayList <Integer> list2 = getArrayList();
 		
 		
-		//mergeSort.sorting(list);
-		bubbleSort.sorting(list);
+		//list2 = mergeSort.sorting(list2);
+		//bubbleSort.sorting(list);
 		
-		comprobeTheResult(list);
+		//comprobeTheResult(list);
+		comprobeTheResultArrayList(list2);
 	}
 	
+	
+	private static ArrayList<Integer> getArrayList(){
+		ArrayList <Integer> list = new ArrayList<Integer>();
+		Random random = new Random();
+		for(int x = 0; x< 10; x++) {
+			list.add(random.nextInt(0, 101));
+		}
+		return list;
+	}
 	
 	private static void comprobeTheResult(Integer[] list) {
 		System.out.print("[");
 		
 		for(int x = 0; x< list.length; x++) {
 			System.out.print(list[x] + ",");
+		}
+		
+		System.out.print("]");
+	}
+	
+	private static void comprobeTheResultArrayList(ArrayList<Integer> list) {
+		System.out.print("[");
+		for(int x = 0; x< list.size(); x++) {
+			System.out.print(list.get(x) + ",");
 		}
 		
 		System.out.print("]");
